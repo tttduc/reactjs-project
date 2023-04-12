54bbbaeef112163b3b6e90867ee5534d6f0eacc1
tttduc <ductrathu@gmail.com>
2023-04-12 19:55:40 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import 'antd/dist/reset.css';
 import "./App.css";
-import { BrowserRouter, Route, Link, Routes } from 'react-router-dom';
-import { Layout, Menu } from 'antd';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
 
 import Categories from './Pages/Categories';
 import Products from './Pages/Products/indexProducts';
-import Employees from './Pages/Employees/indexEmployees';
+/* import Employees from './Pages/Employees/indexEmployees';
 import Suppliers from './Pages/Suppliers/indexSuppliers';
 import Customers from './Pages/Customers/indexCustomers';
-import Orders from './Pages/Orders/indexOrders';
+import Orders from './Pages/Orders/indexOrders'; */
 import Home from './Pages/Home';
 import NavigationBar from './components/NavigationBar';
 
 import numeral from 'numeral';
 import 'numeral/locales/vi';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
 
 const headerStyle: React.CSSProperties = {
   backgroundColor: '#001529',
